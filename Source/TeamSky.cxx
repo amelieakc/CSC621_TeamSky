dab81558e62b73313bfdc58bfa229fa2ab93df04
jmshnds <jmshnds@gmail.com>
2018-04-04 13:22:19 -0700

    Add basic command line menu
    Added basic command line menu to our project, we can use this until we
    add a GUI later.

--- Source/TeamSky.cxx
+++ Source/TeamSky.cxx
@@ -1,15 +1,35 @@
 
 #include "itkImage.h"
+#include <string>
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-  typedef itk::Image< unsigned short, 3 > ImageType;
+	if(argc < 2 || (strcmp(argv[1], "-h") == 0) || strcmp(argv[1], "--help")) {
+		// display help	
+		std::cout << "Team Sky ITK" << std::endl;
+		std::cout << "-h \tdisplay help" << std::endl;
+		std::cout << "-r \tregistration" << std::endl;
+		std::cout << "-s \tsegmentation" << std::endl;
+		// add more options
+	}
+	else if(strcmp(argv[1], "-r") == 0) {
+		std::cout << "Registration" << std::endl;
+		// do regiatration stuff
+	}
+	else if(strcmp(argv[1], "-s") == 0) {
+		std::cout << "Segmentation" << std::endl;
+		// do segmentation stuff
+	}
+	else {
+		std::cout << "Invalid option" << std::endl;
+	}
 
-  ImageType::Pointer image = ImageType::New();
+	// ITK hello world test
+	typedef itk::Image< unsigned short, 3 > ImageType;
+	ImageType::Pointer image = ImageType::New();
+	std::cout << "ITK Hello World !" << std::endl;
 
-  std::cout << "ITK Hello World !" << std::endl;
-
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
